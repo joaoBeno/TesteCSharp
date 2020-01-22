ad53860da9166636cbdef0ee99674c2120b6df1b
João Beno Schreiner Junior <joao@beno.net.br>
2020-01-22 20:53:55 -0300

    Teste 3

--- TesteDotNet/Program.cs
+++ TesteDotNet/Program.cs
@@ -8,5 +8,26 @@ namespace TesteDotNet
         {
             Console.WriteLine("Seja bem vindo!!!");
         }
+
+        static int Soma(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Subtracao(int a, int b)
+        {
+            return a - b;
+        }
+
+        static int Mutiplicacao(int a, int b)
+        {
+            return a * b;
+        }
+
+        static int Divisao(int a, int b)
+        {
+            // Tratar divisão por zero?
+            return a / b;
+        }
     }
 }
