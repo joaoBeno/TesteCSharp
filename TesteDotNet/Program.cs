ec8e747c4ef91d13d44ddd1c53b909601ab72cdd
João Beno Schreiner Junior <joao@beno.net.br>
2020-01-27 01:24:49 -0300

    Teste 4

--- TesteDotNet/Program.cs
+++ TesteDotNet/Program.cs
@@ -11,23 +11,86 @@ namespace TesteDotNet
 
         static int Soma(int a, int b)
         {
-            return a + b;
+            try
+            {
+                return a + b;
+            }
+            catch (Exception e)
+            {
+                throw new ExcecaoDeSoma(e);
+            }
         }
 
         static int Subtracao(int a, int b)
         {
-            return a - b;
+            try
+            {
+                return a - b;
+            }
+            catch (Exception e)
+            {
+                throw new ExcecaoDeSubtracao(e);
+            }
         }
 
         static int Mutiplicacao(int a, int b)
         {
-            return a * b;
+            try
+            {
+                return a * b;
+            }
+            catch (Exception e)
+            {
+                throw new ExcecaoDeMultiplicacao(e);
+            }
         }
 
         static int Divisao(int a, int b)
         {
-            // Tratar divisão por zero?
-            return a / b;
+            try
+            {
+                return a / b;
+            }
+            catch (Exception e)
+            {
+                throw new ExcecaoDeDivisao(e);
+            }
+        }
+
+        private static void SeErro(Exception excecao)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(excecao.Message);
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("\t\t" + excecao.InnerException?.Message ?? "Erro desconhecido");
+            Console.ResetColor();
+        }
+
+        private class ExcecaoDeCalculo: Exception
+        {
+            public ExcecaoDeCalculo(string? message, Exception inner) : base(message, inner)
+            {
+            }
+        }
+
+        private class ExcecaoDeSoma : ExcecaoDeCalculo
+        {
+            public ExcecaoDeSoma(Exception inner) : base("Ocorreu um erro ao tentar somar os valores", inner) {}
+        }
+
+        private class ExcecaoDeSubtracao : ExcecaoDeCalculo
+        {
+            public ExcecaoDeSubtracao(Exception inner) : base("Ocorreu um erro ao tentar subtrair os valores", inner) {}
+        }
+
+        private class ExcecaoDeMultiplicacao : ExcecaoDeCalculo
+        {
+            public ExcecaoDeMultiplicacao(Exception inner) : base("Ocorreu um erro ao tentar multiplicar os valores", inner) {}
+        }
+
+        private class ExcecaoDeDivisao : ExcecaoDeCalculo
+        {
+            public ExcecaoDeDivisao(Exception inner) : base("Ocorreu um erro ao tentar dividir os valores", inner) {}
         }
     }
 }
